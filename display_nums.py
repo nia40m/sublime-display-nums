6141fa6034110b55890084d0a3326e98b739a591
Sokolov Nikita <nia40m@yandex.ru>
2018-10-04 18:28:49 +0300

    WIP
    Signed-off-by: Sokolov Nikita <nia40m@yandex.ru>

--- display_nums.py
+++ display_nums.py
@@ -12,7 +12,7 @@ bin_re = re.compile(r"^0b([01]+)(u|l|ul|lu|ull|llu)?$", re.I)
 def format_str(string, num, separator=" "):
     res = string[-num:]
     string = string[:-num]
-    while (len(string)):
+    while len(string):
         res = string[-num:] + separator + res
         string = string[:-num]
 
@@ -21,27 +21,50 @@ def format_str(string, num, separator=" "):
 def parse_number(text):
     match = dec_re.match(text)
     if match:
-        return int(match.group(1), 10)
+        return int(match.group(1), 10), 10
 
     match = hex_re.match(text)
     if match:
-        return int(match.group(1), 16)
+        return int(match.group(1), 16), 16
 
     match = oct_re.match(text)
     if match:
-        return int(match.group(1), 8)
+        return int(match.group(1), 8), 8
 
     match = bin_re.match(text)
     if match:
-        return int(match.group(1), 2)
+        return int(match.group(1), 2), 2
 
 class DisplayNumberListener(sublime_plugin.EventListener):
-    def on_selection_modified(self, view):
+    def on_selection_modified_async(self, view):
         selected = view.substr(view.sel()[0]).strip()
 
-        selected = parse_number(selected)
-        if selected is None:
+        v = parse_number(selected)
+        if v is None:
             return
+
+        selected, base = v
+
+        positions = ""
+        bit_nums = len("{:b}".format(selected))
+        i = 0
+        while i < bit_nums:
+            positions = "{: =5}".format(i) + positions
+            i += 4
+
+        positions = (" "*4) + positions
+
+        def foo(s, base, num):
+            res = ""
+            offset = 0
+            for c in s[::-1]:
+                if c != " ":
+                    res = """<a href='{{"num":{},"base":{}, "offset":{}}}'>{}</a>""".format(num, base, offset, c) + res
+                    offset += 1
+                else:
+                    res = c + res
+
+            return res
 
         html = """
             <style>
@@ -50,20 +73,29 @@ class DisplayNumberListener(sublime_plugin.EventListener):
                 }}
             </style>
             <body id=show>
-                <div><a href='{{\"num\":{0},\"base\":16}}'>Hex</a>: {1}</div>
-                <div><a href='{{\"num\":{0},\"base\":10}}'>Dec</a>: {2}</div>
-                <div><a href='{{\"num\":{0},\"base\":8}}'>Oct</a>: {3}</div>
-                <div><a href='{{\"num\":{0},\"base\":2}}'>Bin</a>: {4}</div>
+                <div><a href='{{"num":{0},"base":16}}'>Hex</a>: {1}</div>
+                <div><a href='{{"num":{0},"base":10}}'>Dec</a>: {2}</div>
+                <div><a href='{{"num":{0},"base":8}}'>Oct</a>: {3}</div>
+                <div><a href='{{"num":{0},"base":2}}'>Bin</a>: {4}</div>
+                <div>{5}</div>
             </body>
         """.format(
             selected,
             format_str("{:x}".format(selected), 2),
             format_str("{}".format(selected), 3, ","),
             format_str("{:o}".format(selected), 3),
-            format_str("{:b}".format(selected), 4)
+            foo(format_str("{:b}".format(selected), 4), base, selected),
+            positions.replace(" ", "&nbsp;")
         )
 
-        view.show_popup(html, max_width = 1024, on_navigate = lambda x: view.run_command("convert_number", json.loads(x)))
+        def bar(x):
+            data = json.loads(x)
+            if data.get("offset") is None:
+                view.run_command("convert_number", data)
+            else:
+                view.run_command("change_bit", data)
+
+        view.show_popup(html, max_width = 1024, on_navigate = bar)
 
 def convert_number(num, base):
     if base == 10:
@@ -80,3 +112,9 @@ class ConvertNumberCommand(sublime_plugin.TextCommand):
         selected = self.view.sel()[0]
 
         self.view.replace(edit, selected, convert_number(num, base))
+
+class ChangeBitCommand(sublime_plugin.TextCommand):
+    def run(self, edit, num, base, offset):
+        selected = self.view.sel()[0]
+
+        self.view.replace(edit, selected, convert_number(num ^ (1 << offset), base))
