2965f60def9092b54887a4e5d26fce16184d2273
Sokolov Nikita <nia40m@yandex.ru>
2021-04-15 17:07:57 +0300

    refactor: more comments

--- display_nums.py
+++ display_nums.py
@@ -12,10 +12,9 @@ hex_re = re.compile(r"^0x([0-9a-f]+)(u|l|ul|lu|ll|ull|llu)?$", re.I)
 oct_re = re.compile(r"^(0[0-7]+)(u|l|ul|lu|ll|ull|llu)?$", re.I)
 bin_re = re.compile(r"^0b([01]+)(u|l|ul|lu|ll|ull|llu)?$", re.I)
 
-space = "&nbsp;"
-temp_small_space = "*"
-small_space = "<span>"+space+"</span>"
-
+#####
+# Sublime settings getters
+#####
 def get_setting_by_name(project_settings, name):
     if project_settings.has("disnum." + name):
         return project_settings.get("disnum." + name)
@@ -53,6 +52,13 @@ def get_mouse_move_option(project_settings):
         return sublime.HIDE_ON_MOUSE_MOVE_AWAY
 
     return sublime.HIDE_ON_MOUSE_MOVE_AWAY if mouse_move else 0
+
+#####
+# Pop-up string generators
+#####
+space = "&nbsp;"
+temp_small_space = "*"
+small_space = "<span>"+space+"</span>"
 
 def format_str(string, num, separator=" "):
     res = string[-num:]
@@ -99,26 +105,6 @@ def prepare_urls(s, base, num):
             res = c + res
 
     return res
-
-def parse_number(text):
-    # remove underscores in the number
-    text = "".join(split_re.split(text))
-
-    match = dec_re.match(text)
-    if match:
-        return {"number": int(match.group(1), 10), "base": 10}
-
-    match = hex_re.match(text)
-    if match:
-        return {"number": int(match.group(1), 16), "base": 16}
-
-    match = oct_re.match(text)
-    if match:
-        return {"number": int(match.group(1), 8), "base": 8}
-
-    match = bin_re.match(text)
-    if match:
-        return {"number": int(match.group(1), 2), "base": 2}
 
 html_basic = """
 <body id='show'>
@@ -227,6 +213,29 @@ def create_tabled_popup_content(number, hex_num):
 
     return html.replace(" ", space)
 
+#####
+# Main listener of selection event
+#####
+def parse_number(text):
+    # remove underscores in the number
+    text = "".join(split_re.split(text))
+
+    match = dec_re.match(text)
+    if match:
+        return {"number": int(match.group(1), 10), "base": 10}
+
+    match = hex_re.match(text)
+    if match:
+        return {"number": int(match.group(1), 16), "base": 16}
+
+    match = oct_re.match(text)
+    if match:
+        return {"number": int(match.group(1), 8), "base": 8}
+
+    match = bin_re.match(text)
+    if match:
+        return {"number": int(match.group(1), 2), "base": 2}
+
 class DisplayNumberListener(sublime_plugin.EventListener):
     def on_selection_modified_async(self, view):
         # if more then one select close popup
@@ -270,6 +279,9 @@ class DisplayNumberListener(sublime_plugin.EventListener):
             on_navigate = select_function
         )
 
+#####
+# Sublime commands
+#####
 def convert_number(num, base):
     if base == 10:
         return "{:d}".format(num)
